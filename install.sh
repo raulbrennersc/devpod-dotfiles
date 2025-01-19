29aeaded560e316444f1303f8fb09543a9d1c959
Raul Costa <raulbrennersc@gmail.com>
2025-01-19 11:51:51 -0300

    add neovim

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+export XDG_CONFIG_HOME="$HOME"/.config
+
 curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/raulbrennersc/dotfiles/main/.zshrc
+
+
+sudo dnf install fzf ripgrep neovim -y
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
 
