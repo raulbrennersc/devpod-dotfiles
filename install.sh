b03c890b64bf4ca0419c352a64bc7a77c81a75c6
Raul Costa <raulbrennersc@gmail.com>
2025-01-19 13:52:13 -0300

    fix nvchad clone

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew install fzf ripgrep neovim
 
-git clone https://github.com/NvChad/NvChad ~/.config/nvchad
+git clone https://github.com/NvChad/starter ~/.config/nvchad
 git clone https://github.com/LazyVim/starter ~/.config/lazyvim
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
