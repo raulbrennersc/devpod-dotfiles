39e246bc7b16095102b264a0efab420fa9dff5db
Raul Costa <raulbrennersc@gmail.com>
2025-01-19 12:26:56 -0300

    add nvim distros

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ export XDG_CONFIG_HOME="$HOME"/.config
 
 curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/raulbrennersc/dotfiles/main/.zshrc
 
-
 sudo dnf install fzf ripgrep neovim -y
+
+git clone --depth 1 https://github.com/NvChad/NvChad ~/.config/nvchad
+git clone --depth 1 https://github.com/LazyVim/starter ~/.config/lazyvim
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
