496e73b74d88c96da2533ae6b7c52337b5e67e0b
Raul Costa <raulbrennersc@gmail.com>
2025-01-19 12:37:55 -0300

    fix install command

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ export XDG_CONFIG_HOME="$HOME"/.config
 
 curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/raulbrennersc/dotfiles/main/.zshrc
 
-sudo dnf install fzf ripgrep neovim -y
+sudo apt update
+sudo apt install fzf ripgrep neovim -y
 
 git clone --depth 1 https://github.com/NvChad/NvChad ~/.config/nvchad
 git clone --depth 1 https://github.com/LazyVim/starter ~/.config/lazyvim
