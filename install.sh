fa1758753411a2c4d80a8bb06f86ce1121b25337
Raul Costa <raulbrennersc@gmail.com>
2025-01-19 13:23:18 -0300

    use brew

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ export XDG_CONFIG_HOME="$HOME"/.config
 
 curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/raulbrennersc/dotfiles/main/.zshrc
 
-sudo apt update
-sudo apt install fzf ripgrep neovim -y
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install fzf ripgrep neovim
 
 git clone --depth 1 https://github.com/NvChad/NvChad ~/.config/nvchad
 git clone --depth 1 https://github.com/LazyVim/starter ~/.config/lazyvim
