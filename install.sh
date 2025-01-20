9a32cd2df9859309ebf2a9d898a0a7c4a8c70d9e
Raul Costa <raulbrennersc@gmail.com>
2025-01-20 18:07:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 export XDG_CONFIG_HOME="$HOME"/.config
 
 curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/raulbrennersc/dotfiles/main/.zshrc
+curl -o "$HOME/.gitconfig" https://raw.githubusercontent.com/raulbrennersc/dotfiles/main/.gitconfig
+
 cp "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
