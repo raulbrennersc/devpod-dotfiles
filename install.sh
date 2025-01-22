787025c134014dd910492ef17ea56db038149cdb
Raul Costa <raulbrennersc@gmail.com>
2025-01-22 14:15:02 -0300

    add oh-my-posh from brew

--- install.sh
+++ install.sh
@@ -9,11 +9,8 @@ cp "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-brew install fzf ripgrep neovim tmux
+brew install fzf ripgrep neovim tmux oh-my-posh
 
 git clone https://github.com/NvChad/starter ~/.config/nvchad
 git clone https://github.com/LazyVim/starter ~/.config/lazyvim
-
-curl -s https://ohmyposh.dev/install.sh | bash -s
-
 
